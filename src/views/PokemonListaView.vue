ddc44cd771dea28a762600a6fb819298e56d74d1
EnzoGabrielBarbosaFranco <enzoddos7@gmail.com>
2022-10-05 13:05:34 -0400

    botão delete

--- src/views/PokemonListaView.vue
+++ src/views/PokemonListaView.vue
@@ -1,10 +1,14 @@
 
 <script >
+import PokemonRequest from '../models/PokemonRequest';
 import PokemonDataService from '../services/PokemonDataService';
 export default {
   name: "pokemons-lista",
   data() {
-    return { pokemons: [] };
+    return {
+      pokemons: [],
+      pokemonSelecionado: this.inicializaPokemon()
+    };
   },
   methods: {
     buscarPokemons() {
@@ -16,7 +20,31 @@ export default {
         .catch(erro => {
           console.log(erro);
         });
-    }
+    },
+    removerPokemonSelecionado() {
+      PokemonDataService.remover(this.pokemonSelecionado.id)
+        .then(() => {
+          this.pokemons = this.pokemons.filter(pokemon => pokemon.id != this.pokemonSelecionado.id);
+          this.inicializaPokemon();
+        })
+        .catch(() => {
+          this.inicializaPokemon();
+        });
+    },
+    inicializaPokemon() {
+      return {
+        "id": null,
+        "nome": null
+      }
+    },
+    selecionar(pokemon) {
+      this.pokemonSelecionado.id = pokemon.id;
+      this.pokemonSelecionado.nome = pokemon.nome;
+     },
+    // editar(id) {
+    //   this.$router.push({ name: 'pokemon-edit', params: {id: id}});
+    // }
+
   },
   mounted() {
     this.buscarPokemons();
@@ -87,7 +115,8 @@ export default {
                     </div>
                   </div>
                   <div class="text-center">
-                    <button type="button" class="btn btn-outline-primary pt-1 m-1 " data-bs-toggle="collapse" :data-bs-target="'#collapseExample' + pokemon.id">
+                    <button type="button" class="btn btn-outline-primary pt-1 m-1 " data-bs-toggle="collapse"
+                      :data-bs-target="'#collapseExample' + pokemon.id">
                       Detalhes
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                         class="bi bi-plus-lg" viewBox="0 0 16 16">
@@ -104,14 +133,23 @@ export default {
                           d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
                       </svg>
                     </button>
-                    <button type="button" class="btn btn-outline-danger m-1" data-bs-toggle="modal"
-                      data-bs-target="#confirmacaoExclusao">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                        class="bi bi-trash3" viewBox="0 0 16 16">
+                    <!-- <button id="btn-editar" type="button" class="btn btn-outline-secondary m-1"
+                      @click="editar(pokemon.id)"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                        fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                         <path
-                          d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
-                      </svg>
-                    </button>
+                          d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                        <path fill-rule="evenodd"
+                          d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                      </svg> </button> -->
+                    <button type="button" @click="selecionar(pokemon)" class="btn btn-outline-danger m-1"
+                      data-bs-toggle="modal" data-bs-target="#confirmacaoExclusaoPokemon" data-bs-whatever="@mdo"><svg
+                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-trash" viewBox="0 0 16 16">
+                        <path
+                          d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                        <path fill-rule="evenodd"
+                          d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                      </svg></button>
                   </div>
                 </div>
               </div>
@@ -119,8 +157,7 @@ export default {
           </div>
         </div>
       </div>
-      <button type="button"
-        class="btn btn-primary pt-1 m-1">
+      <button type="button" class="btn btn-primary pt-1 m-1">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg"
           viewBox="0 0 16 16">
           <path fill-rule="evenodd"
@@ -128,6 +165,26 @@ export default {
         </svg>
         Novo Pokémon
       </button>
+    </div>
+    <div class="modal fade" id="confirmacaoExclusaoPokemon" tabindex="-1" aria-labelledby="exampleModalLabel"
+      aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">VOCÊ ESTÁ APAGANDO UM
+              POKÉMON!!</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            Você está apagando o Pokémon "<strong> {{pokemonSelecionado.nome}} </strong>". Deseja continuar?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-success" @click="removerPokemonSelecionado"
+              data-bs-dismiss="modal">Continuar</button>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
